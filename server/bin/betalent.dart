65267a1b3e31736ea707a1f17400c157bb95c2ab
Mathvdias <mattmvc56@gmail.com>
2024-11-21 15:55:00 -0300

    foundation stone

---
+++ server/bin/betalent.dart
@@ -0,0 +1,5 @@
+import 'package:betalent/betalent.dart' as betalent;
+
+void main(List<String> arguments) {
+  print('Hello world: ${betalent.calculate()}!');
+}
---
+++ server/test/betalent_test.dart
@@ -0,0 +1,8 @@
+import 'package:betalent/betalent.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
