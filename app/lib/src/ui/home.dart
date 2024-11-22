5b8d372c7d9e2b16502c8cd486b6482328572216
Mathvdias <mattmvc56@gmail.com>
2024-11-22 13:39:55 -0300

    implement focusnode in keyboard

--- app/lib/src/ui/home.dart
+++ app/lib/src/ui/home.dart
@@ -4,6 +4,8 @@ import 'package:app/src/data/entities/employer.dart';
 import 'package:app/src/di/di.dart';
 import 'package:app/src/ui/betalent_svg.dart';
 import 'package:app/src/ui/states/base.dart';
+import 'package:app/src/ui/widgets/employee_data.dart';
+import 'package:app/src/ui/widgets/header.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -15,277 +17,143 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: AppTheme.whiteColor,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(18.0, 18.0, 0.0, 0.0),
-                  child: CircleAvatar(
-                    backgroundColor: AppTheme.greyColor,
-                    child: Text(
-                      'GC',
-                      style: Theme.of(context).textTheme.bodyLarge,
+      body: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(18.0, 18.0, 0.0, 0.0),
+                    child: CircleAvatar(
+                      backgroundColor: AppTheme.greyColor,
+                      child: Text(
+                        'GC',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0, 18.0, 18.0, 0.0),
-                  child: IconButton(
-                    icon: Badge.count(
-                      backgroundColor: AppTheme.primaryBlue,
-                      count: 02,
-                      child: BeTalentSvg.notification,
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 18.0, 18.0, 0.0),
+                    child: IconButton(
+                      icon: Badge.count(
+                        backgroundColor: AppTheme.primaryBlue,
+                        count: 02,
+                        child: BeTalentSvg.notification,
+                      ),
+                      onPressed: () {},
                     ),
-                    onPressed: () {},
                   ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: FutureBuilder(
-                future: controller.call(),
-                builder: (context, snapshot) {
-                  final state = controller.state;
+                ],
+              ),
+              Expanded(
+                child: FutureBuilder(
+                  future: controller.call(),
+                  builder: (context, snapshot) {
+                    final state = controller.state;
 
-                  if (state is LoadingState) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (state is SuccessState<List<Employee>>) {
-                    final employees = state.data;
+                    if (state is LoadingState) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (state is SuccessState<List<Employee>>) {
+                      final employees = state.data;
 
-                    return Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(18.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(bottom: 8.0),
-                                child: Text(
-                                  'Funcionários',
-                                  style:
-                                      Theme.of(context).textTheme.displaySmall,
+                      return Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(18.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 8.0),
+                                  child: Text(
+                                    'Funcionários',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .displaySmall,
+                                  ),
                                 ),
-                              ),
-                              SearchAnchor(
-                                builder: (
-                                  BuildContext context,
-                                  SearchController searchController,
-                                ) {
-                                  return SearchBar(
-                                    elevation: const WidgetStatePropertyAll(0),
-                                    backgroundColor: WidgetStateProperty.all(
-                                        AppTheme.greyColor),
-                                    controller: searchController,
-                                    hintText: 'Pesquisar',
-                                    onChanged: (query) {
-                                      searchController.openView();
-                                      controller.search(query);
-                                    },
-                                    leading: const Icon(Icons.search),
-                                    padding: const WidgetStatePropertyAll<
-                                        EdgeInsets>(
-                                      EdgeInsets.symmetric(horizontal: 16.0),
-                                    ),
-                                  );
-                                },
-                                suggestionsBuilder: (
-                                  BuildContext context,
-                                  SearchController searchController,
-                                ) {
-                                  final query =
-                                      searchController.text.toLowerCase();
-
-                                  final filteredEmployees =
-                                      controller.search(query);
-
-                                  if (filteredEmployees.isEmpty) {
-                                    return [
-                                      const Center(
-                                        child: Padding(
-                                          padding: EdgeInsets.all(16.0),
-                                          child: Text(
-                                              'Nenhum funcionário encontrado'),
-                                        ),
+                                SearchAnchor(
+                                  builder: (
+                                    BuildContext context,
+                                    SearchController searchController,
+                                  ) {
+                                    return SearchBar(
+                                      elevation:
+                                          const WidgetStatePropertyAll(0),
+                                      backgroundColor: WidgetStateProperty.all(
+                                          AppTheme.greyColor),
+                                      controller: searchController,
+                                      hintText: 'Pesquisar',
+                                      onSubmitted: (x) =>
+                                          searchController.clear(),
+                                      onChanged: (query) {
+                                        searchController.openView();
+                                        controller.search(query);
+                                      },
+                                      leading: const Icon(Icons.search),
+                                      padding: const WidgetStatePropertyAll<
+                                          EdgeInsets>(
+                                        EdgeInsets.symmetric(horizontal: 16.0),
                                       ),
-                                    ];
-                                  }
+                                    );
+                                  },
+                                  suggestionsBuilder: (
+                                    BuildContext context,
+                                    SearchController searchController,
+                                  ) {
+                                    final query =
+                                        searchController.text.toLowerCase();
 
-                                  return filteredEmployees.map((employee) {
-                                    return EmployeeData(employee: employee);
-                                  }).toList();
-                                },
-                              ),
-                            ],
+                                    final filteredEmployees =
+                                        controller.search(query);
+
+                                    if (filteredEmployees.isEmpty) {
+                                      return [
+                                        const Center(
+                                          child: Padding(
+                                            padding: EdgeInsets.all(16.0),
+                                            child: Text(
+                                                'Nenhum funcionário encontrado'),
+                                          ),
+                                        ),
+                                      ];
+                                    }
+
+                                    return filteredEmployees.map((employee) {
+                                      return EmployeeData(employee: employee);
+                                    }).toList();
+                                  },
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        const HeaderEmployee(),
-                        Expanded(
-                          child: ListView.builder(
-                            itemCount: employees.length,
-                            itemBuilder: (context, index) {
-                              final employee = employees[index];
-                              return EmployeeData(employee: employee);
-                            },
+                          const HeaderEmployee(),
+                          Expanded(
+                            child: ListView.builder(
+                              itemCount: employees.length,
+                              itemBuilder: (context, index) {
+                                final employee = employees[index];
+                                return EmployeeData(employee: employee);
+                              },
+                            ),
                           ),
-                        ),
-                      ],
-                    );
-                  } else if (state is ErrorState) {
-                    return Center(child: Text('Erro: ${state.exception}'));
-                  }
+                        ],
+                      );
+                    } else if (state is ErrorState) {
+                      return Center(child: Text('Erro: ${state.exception}'));
+                    }
 
-                  return const Center(child: Text('Sem dados disponíveis'));
-                },
+                    return const Center(child: Text('Sem dados disponíveis'));
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class HeaderEmployee extends StatelessWidget {
-  const HeaderEmployee({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 12.5),
-      decoration: BoxDecoration(
-        color: AppTheme.headerColor,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(8.0),
-          topRight: Radius.circular(8.0),
-        ),
-        border: Border.all(
-          color: AppTheme.borderColor,
-          width: 1,
-        ),
-      ),
-      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      child: Row(
-        children: [
-          const Expanded(
-            child: Text(
-              "Foto",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const Expanded(
-            flex: 4,
-            child: Text(
-              "Nome",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: BeTalentSvg.dot,
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class EmployeeData extends StatelessWidget {
-  const EmployeeData({
-    super.key,
-    required this.employee,
-  });
-
-  final Employee employee;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 12.5),
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(color: AppTheme.borderColor),
-          right: BorderSide(color: AppTheme.borderColor),
-          left: BorderSide(color: AppTheme.borderColor),
-        ),
-      ),
-      child: ExpansionTile(
-        childrenPadding: const EdgeInsets.symmetric(horizontal: 24.0),
-        trailing: BeTalentSvg.arrowDown,
-        leading: CircleAvatar(
-          backgroundColor: Colors.grey[300],
-          child: ClipOval(
-            child: Image.network(
-              employee.image,
-              width: 40,
-              height: 40,
-              fit: BoxFit.cover,
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) {
-                  return child;
-                } else {
-                  return const Icon(Icons.person, color: Colors.white);
-                }
-              },
-              errorBuilder: (context, error, stackTrace) {
-                return const Icon(Icons.error, color: Colors.red);
-              },
-            ),
-          ),
-        ),
-        title: Text(employee.name),
-        controlAffinity: ListTileControlAffinity.trailing,
-        children: <Widget>[
-          ItemTileData(
-            employeeData: employee.job,
-            label: 'Cargo',
-          ),
-          ItemTileData(
-            employeeData: employee.admissionDateFormatted,
-            label: 'Data de admissão',
-          ),
-          ItemTileData(
-            employeeData: employee.phoneFormatted,
-            label: 'Telefone',
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ItemTileData extends StatelessWidget {
-  const ItemTileData({
-    super.key,
-    required this.employeeData,
-    required this.label,
-  });
-
-  final String employeeData;
-  final String label;
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          label,
-          style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-              ),
-        ),
-        Text(employeeData),
-      ],
     );
   }
 }
